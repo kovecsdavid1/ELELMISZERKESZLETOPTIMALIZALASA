9d581ecf246d880550654b9eaf7e53351953263e
kovecsdavid1 <kovecsdavid05@gmail.com>
2025-05-06 12:07:58 +0200

    FoodItemController létrehozása és implementálása

---
+++ BACKEND/BACKEND/BACKEND/Controllers/FoodItemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FoodItemController : Controller
+    {
+        [HttpPost]
+        public IActionResult OptimizeInventory([FromBody])
+        {
+            return View();
+        }
+    }
+}
