f07e44127f9615efa72d800d77c78dd7dc76de35
kovecsdavid1 <kovecsdavid05@gmail.com>
2025-05-06 11:29:31 +0200

    FoodItem osztály létrehozása

---
+++ BACKEND/BACKEND/BACKEND/Models/FoodItem.cs
@@ -0,0 +1,9 @@
+namespace BACKEND
+{
+    public class FoodItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
