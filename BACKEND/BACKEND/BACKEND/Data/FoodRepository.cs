4aca4fc2f69c5de8e4420d1ddaeb3c26797824e8
kovecsdavid1 <kovecsdavid05@gmail.com>
2025-05-06 12:08:45 +0200

    FoodRepository létrehozása és implementálása

---
+++ BACKEND/BACKEND/BACKEND/Data/FoodRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace BACKEND.Data
+{
+    public class FoodRepository : IFoodRepository
+    {
+        private readonly List<FoodItem> _items = new List<FoodItem>();
+
+        public List<FoodItem> GetAll()
+        {
+            return _items;
+        }
+
+        public void Add(FoodItem item)
+        {
+            _items.Add(item);
+        }
+
+        public void Remove(FoodItem item)
+        {
+            _items.Remove(item);
+        }
+    }
+}
