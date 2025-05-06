d0786964e2b620f9a894c1f13c86ca541d9d47ef
kovecsdavid1 <kovecsdavid05@gmail.com>
2025-05-06 16:11:01 +0200

    Nagy módosítás előtti mentés

--- BACKEND/BACKEND/BACKEND/Data/IFoodRepository.cs
+++ BACKEND/BACKEND/BACKEND/Data/IFoodRepository.cs
@@ -6,6 +6,9 @@ namespace BACKEND.Data
     public interface IFoodRepository
     {
         List<FoodItem> GetAll();
+
+        public List<FoodItem> validItems(List<FoodItem> foodItems);
+
         void Add(FoodItem item);
         void Remove(FoodItem item);
     }
